846301336edfcbfde9573d6ed87063d23ebca2b3
nikhildubey-23 <ravannick0089@gmail.com>
2026-04-13 12:57:49 +0530

    sitemap file

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
 
@@ -9,6 +9,14 @@ def index():
 @app.route('/approvals')
 def approvals():
     return render_template('approvals.html')
+
+@app.route('/robots.txt')
+def robots():
+    return send_from_directory('static', 'robots.txt', mimetype='text/plain')
+
+@app.route('/sitemap.xml')
+def sitemap():
+    return send_from_directory('static', 'sitemap.xml', mimetype='application/xml')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
